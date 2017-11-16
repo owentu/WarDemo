7ccb91eeab7a0ac0a3e3a2f03105ea2527f7cae1
DESKTOP-FPCR4BL\Owen <owen.tu@ixorp.com>
2017-11-16 10:34:24 +0800

    no message

--- WarDemo/src/com/ixorp/edi/controller/GetPackageUnit.java
+++ WarDemo/src/com/ixorp/edi/controller/GetPackageUnit.java
@@ -45,9 +45,9 @@ public class GetPackageUnit extends HttpServlet {
 		// TODO Auto-generated method stub
 		JSONObject json = new JSONObject();
 		
-		json.put("CT", "CARTONS");
+		json.put("CT", "CARTON");
 		json.put("PC", "PIECE");
-		json.put("BG", "BAGS");
+		json.put("BG", "BAG");
 		json.put("DR", "DRUM");
 		json.put("BX", "BOX");
 		
